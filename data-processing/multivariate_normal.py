b57c38d4c3ae1c2c04d8b6f399524691d0bd598f
Oh-Hun Kwon <kirloyal@gmail.com>
2019-04-17 14:40:02 +0200

    multivariate normal

---
+++ data-processing/multivariate_normal.py
@@ -0,0 +1,21 @@
+#%%
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data  # (150, 4)
+y = iris.target # (150,)
+
+#%%
+
+classes = list(set(y.flatten()))
+for i in range(len(classes)):
+    y_tar = classes[i]
+    mu = np.mean(X[y==y_tar], axis=0)
+    cov = np.cov(X[y==y_tar], rowvar=False)
+    print(mu)
+    print(cov)
+
+#%%
