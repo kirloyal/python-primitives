193e52061e2a939a9897de92c2e55f9864207f45
Oh-Hun Kwon <kirloyal@gmail.com>
2019-07-08 17:04:20 +0200

    save checkpoint

--- pytorch/framework_train.py
+++ pytorch/framework_train.py
@@ -21,13 +21,14 @@ device = torch.device("cuda" if use_cuda else "cpu")
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
 parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N', help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N', help='number of epochs to train (default: 10)')
+parser.add_argument('--epochs', type=int, default=5, metavar='N', help='number of epochs to train (default: 10)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR', help='learning rate (default: 0.01)')
 parser.add_argument('--momentum', type=float, default=0.5, metavar='M', help='SGD momentum (default: 0.5)')
 parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
-parser.add_argument('--save-model', action='store_true', default=False, help='For Saving the current Model')
+parser.add_argument('--load-model', type=str, default='', help='For Loading the saved Model')
+parser.add_argument('--save-model', type=str, default='./tmp/mnist_cnn.pt', help='For Saving the current Model')
 args = parser.parse_args()
 
 torch.manual_seed(args.seed)
@@ -44,11 +45,21 @@ model = Net().to(device)
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 criterion = F.nll_loss
 
+epoch_start = 0
+best_test_loss = float("inf")
+if args.load_model != '':
+    state = torch.load(args.load_model)
+    model.load_state_dict(state['model_state_dict'])
+    optimizer.load_state_dict(state['optimizer_state_dict'])
+    epoch_start = state['epoch']
+    best_test_loss = state['loss']
+
 writer_correct = SummaryWriter('./tmp/correct')
 writer_train = SummaryWriter('./tmp/train_0')
 writer_test = SummaryWriter('./tmp/test_0')
 
-for epoch in range(1, args.epochs + 1):
+
+for epoch in range(epoch_start + 1, epoch_start + args.epochs + 1):
     model.train()
     pbar = tqdm(train_loader)
     train_loss = 0
@@ -84,6 +95,17 @@ for epoch in range(1, args.epochs + 1):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
+    if test_loss < best_test_loss:
+        print(epoch)
+        best_test_loss = test_loss
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'loss': test_loss
+            }, args.save_model)
+
 
 writer_train.close()
 writer_test.close()
