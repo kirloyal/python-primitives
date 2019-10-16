ffad2f5ab145a2f7cbc1b57483ff99b6487ad99c
Oh-Hun Kwon <kirloyal@gmail.com>
2019-10-17 00:48:19 +0200

    3d lines

--- matplotlib/plot_lines.py
+++ matplotlib/plot_lines.py
@@ -13,3 +13,20 @@ line_segments = LineCollection(lines, linewidths=0.5, colors=colors, linestyles=
 ax.add_collection(line_segments)
 ax.set_title('draw multi lines')
 plt.show()
+
+#%%
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+from matplotlib import colors as mcolors
+
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+colors = np.array([(1., 0., 0., 1.), (0., 1., 0., 1.)])
+lines = np.random.random((2,4,3))
+line_segments = Line3DCollection(lines, linewidths=0.5, colors=colors, linestyles='solid')
+ax.add_collection(line_segments)
+ax.set_title('draw multi lines')
+plt.show()
